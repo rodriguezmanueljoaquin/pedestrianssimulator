3e737c40bf565beff3e2b8e4850a2ded86b51b61
manurodriguez <manurodriguez@itba.edu.ar>
2023-01-07 21:11:15 -0300

    Improvement when considering originalDirection for the heuristic

--- src/OperationalModelModule/CPM.java
+++ src/OperationalModelModule/CPM.java
@@ -13,53 +13,45 @@ public class CPM {
     private static final double EXPANSION_TIME = 0.5;
     private static final double MAX_SPEED = 3.0;
     private static final double NEIGHBOURS_RADIUS = 3.0;
-    private static final double HEURISTIC_WEIGHT = 0.4;
-    private static final double agentAp = 1200, agentBp = 0.6, wallAp = 100, wallBp = 0.1, beta = .9, tau = .5;
+    private static final double ORIGINAL_DIRECTION_AP = 200, AGENT_AP = 400, AGENT_BP = 0.5, WALL_AP = 100, WALL_BP = 0.5, BETA = .9, TAU = .5;
     // TODO: MAPA<ID DE AGENTE, CPMAGENT> PARA ASOCIAR COEFICIENTES DISTINTOS (randomizados un poco desde un valor) A LOS AGENTS
 
     public static void updateAgent(Agent agent, List<Agent> agents, Environment environment) {
-        Vector heuristicVelocity = calculateHeuristicVelocity(agent, agents, environment);
-        Vector resultantVelocity =
-                calculateTargetDirection(heuristicVelocity, agent.getVelocity()).scalarMultiply(agent.getState().getVelocity()); // takes in account both heuristic velocity and original agent velocity
-//        if(heuristicVelocity.module() != 0.0)
-//            System.out.println(heuristicVelocity);
-        agent.setVelocity(resultantVelocity);
+        Vector heuristicDirection = calculateHeuristicDirection(agent, agents, environment);
+        agent.setVelocity(heuristicDirection.scalarMultiply(agent.getState().getVelocity()));
     }
 
-    private static Vector calculateHeuristicVelocity(Agent agent, List<Agent> agents, Environment environment) {
-        Vector resultantNc = new Vector(0, 0);
+    private static Vector calculateHeuristicDirection(Agent agent, List<Agent> agents, Environment environment) {
+        Vector resultantNc = agent.getVelocity().normalize().scalarMultiply(ORIGINAL_DIRECTION_AP); //initialize with original direction
+
         List<Agent> neighbours = agents.stream()
                 .filter(other -> agent.getPosition().distance(other.getPosition()) < NEIGHBOURS_RADIUS && !agent.equals(other))
                 .collect(Collectors.toList());
 
         for (Agent neighbour : neighbours) {
-            resultantNc = resultantNc.add(calculateRepulsionForce(agent.getPosition(), neighbour.getPosition(), agent.getVelocity(), agentAp, agentBp));
+            resultantNc = resultantNc.add(calculateRepulsionForce(agent.getPosition(), neighbour.getPosition(), agent.getVelocity(), AGENT_AP, AGENT_BP));
         }
-//        Vector closestWallPosition = environment.getClosestWall(agent.getPosition()).getClosestPoint(agent.getPosition());
-//        Vector wallRepulsion = calculateRepulsionForce(agent.getPosition(),closestWallPosition,agent.getVelocity(),wallAp,wallBp);
-        return resultantNc;
+
+        Vector closestWallPosition = environment.getClosestWall(agent.getPosition()).getClosestPoint(agent.getPosition());
+        Vector wallRepulsion = calculateRepulsionForce(agent.getPosition(),closestWallPosition,agent.getVelocity(), WALL_AP, WALL_BP);
+        resultantNc = resultantNc.add(wallRepulsion);
+
+        return resultantNc.normalize();
     }
 
-    private static Vector calculateTargetDirection(Vector heuristicVelocity, Vector originalVelocity) {
-        Vector heuristicWeightedVelocity = heuristicVelocity.normalize().scalarMultiply(HEURISTIC_WEIGHT);
-        Vector originalWeightedVelocity = originalVelocity.normalize().scalarMultiply(1.0 - HEURISTIC_WEIGHT);
-        return heuristicWeightedVelocity.add(originalWeightedVelocity).normalize();
-    }
-
-
-    private static Vector calculateRepulsionForce(Vector position, Vector obstacle, Vector targetVelocity, double Ap, double Bp) {
+    private static Vector calculateRepulsionForce(Vector position, Vector obstacle, Vector originalVelocity, double Ap, double Bp) {
         //eij (e sub ij)
         Vector repulsionDirection = position.substract(obstacle).normalize();
 
         //dij (d sub ij) distance between position and otherPosition
-        Double repulsionDistance = obstacle.distance(position);
+        Double repulsionDistance = position.distance(obstacle);
 
-//       Vector objectiveVector = objective.substract(position).normalize();
-        Vector targetDirection = targetVelocity.normalize();
-        //cos(0) = a.b / |a||b| (ya estan normalizados o sea |a| = |b| = 1)
-        double cosineOfTheta = targetVelocity.add(position).dotMultiply(obstacle) / (targetVelocity.add(position).module() * obstacle.module());
+        //cos(0) = a.b / |a||b|
+        double cosineOfTheta = originalVelocity.add(position).dotMultiply(obstacle) / (originalVelocity.add(position).module() * obstacle.module());
+        double weight = Ap * Math.exp(-repulsionDistance / Bp);
+
         //eij*Ap*e^(-dij/bp)*cos(0)
-        return repulsionDirection.scalarMultiply(Ap * Math.exp(-repulsionDistance / Bp) * cosineOfTheta);
+        return repulsionDirection.scalarMultiply(weight * cosineOfTheta);
     }
 
 }
