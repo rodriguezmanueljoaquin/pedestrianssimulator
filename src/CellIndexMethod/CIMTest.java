3740ac4bac7c76d88dfd9b876e9c106e230f3e60
manurodriguez <manurodriguez@itba.edu.ar>
2023-01-17 22:43:17 -0300

    Add CIMTest

---
+++ src/CellIndexMethod/CIMTest.java
@@ -0,0 +1,41 @@
+package CellIndexMethod;
+
+import Agent.Agent;
+import AgentsBehaviour.StateMachine.StateMachine;
+import AgentsBehaviour.StateMachine.SuperMarketClientSM;
+import Environment.Wall;
+import GraphGenerator.Graph;
+import Utils.InputHandler;
+import Utils.Vector;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CIMTest {
+    public static void main(String[] args) {
+        List<Wall> walls = new ArrayList<>();
+        walls.add(new Wall(new Vector(0., 0.), new Vector(50, 0)));
+        walls.add(new Wall(new Vector(50., 0.), new Vector(50, 20)));
+        walls.add(new Wall(new Vector(50., 20.), new Vector(0, 20)));
+        walls.add(new Wall(new Vector(0., 20.), new Vector(0, 0)));
+        StateMachine stateMachine = new SuperMarketClientSM(new Graph(walls));
+
+        List<Agent> agents = new ArrayList<>();
+        Agent analyzedAgent = new Agent(new Vector(10,10), 0.1, stateMachine, new ArrayList<>());
+
+        agents.add(analyzedAgent);
+        agents.add(new Agent(new Vector(5,5), 0.1, stateMachine, new ArrayList<>()));
+        agents.add(new Agent(new Vector(10,5), 0.1, stateMachine, new ArrayList<>()));
+        agents.add(new Agent(new Vector(10,15), 0.1, stateMachine, new ArrayList<>()));
+        agents.add(new Agent(new Vector(5,10), 0.1, stateMachine, new ArrayList<>()));
+        agents.add(new Agent(new Vector(15,10), 0.1, stateMachine, new ArrayList<>()));
+        agents.add(new Agent(new Vector(15,15), 0.1, stateMachine, new ArrayList<>()));
+        CellIndexMethod cim = new CellIndexMethod(walls, 50);
+        cim.updateAgentsPosition(agents);
+
+        List<Agent> neighbours = cim.getAgentNeighbours(analyzedAgent);
+        System.out.println(neighbours.stream().map(Agent::getId).sorted().collect(Collectors.toList()));
+    }
+}
