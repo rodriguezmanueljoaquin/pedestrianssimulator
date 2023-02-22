91425fed62a348d8ba0886d9382743f732f3cdda
Gaston Donikian <gdonikian@itba.edu.ar>
2023-02-22 18:34:34 -0300

    Hot fix exponential distribution

--- src/main/java/Utils/Random/ExponentialRandom.java
+++ src/main/java/Utils/Random/ExponentialRandom.java
@@ -1,22 +1,23 @@
 package Utils.Random;
 
 public class ExponentialRandom implements RandomInterface{
-    double lambda;
+    double alpha;
 
-    public ExponentialRandom(double lambda){
-        this.lambda = lambda;
+    public ExponentialRandom(double alpha){
+        this.alpha = alpha;
     }
-
+    //ALPHA IS STD, it is usually defined with lambda = 1/alpha;
+    //However, it is more intuitive for the end user to define the STD
     @Override
     public Double getNewRandomNumber() {
         //As Math.random() returns a number between [0, 1)
         //Notice the parenthesis
         //I have to do 1 - Math.random() so as to not get a log(0)
-       return Math.log(1 - Math.random())/(-lambda);
+       return Math.log(1 - Math.random())*(-alpha);
     }
 
     @Override
     public Double getMean(){
-        return 1/lambda;
+        return alpha;
     }
 }
