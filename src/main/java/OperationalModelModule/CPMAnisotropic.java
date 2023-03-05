dde7c959206a20e46d2ad3112b21c2a791a1138c
Gaston Donikian <gdonikian@itba.edu.ar>
2023-03-05 15:06:16 -0300

    Added geometric handling of CPM-A

--- src/main/java/OperationalModelModule/CPMAnisotropic.java
+++ src/main/java/OperationalModelModule/CPMAnisotropic.java
@@ -4,6 +4,7 @@ import Agent.Agent;
 import Environment.Environment;
 import OperationalModelModule.Collisions.AgentsCollision;
 import OperationalModelModule.Collisions.WallCollision;
+import Utils.Vector;
 
 import java.util.List;
 import java.util.Random;
@@ -20,7 +21,7 @@ public class CPMAnisotropic implements OperationalModelModule{
 
         //Colliding conditions with agents greatly complicates
         //if agent.getRadius() == r min and agent.distance(otherAgent) < 0
-            //collisio
+            //collision
         //if agent.getRadius() != r min and isInFieldOfView &&
         //lines parallel to agent direction from both borders of r min
         //any of those intersects with the current radius of agent
@@ -47,5 +48,42 @@ public class CPMAnisotropic implements OperationalModelModule{
     @Override
     public void updateNonCollisionAgent(Agent agent, double dt, Random random) {
 
+    }
+
+    private static double getBeta(Agent agent, Agent otherAgent) {
+        //As PDF says, Beta = the angle formed by speed of agent and the relative direction between other agent and agent
+        Vector relativeDirection = otherAgent.getPosition().substract(agent.getPosition());
+        Vector agentDirection = agent.getDirection();
+        // We use acos to calculate this direction
+        return Math.acos(agentDirection.dotMultiply(relativeDirection)/(agentDirection.module()* relativeDirection.module());
+    }
+
+    public static boolean isInContact(Agent agent, Agent otherAgent) {
+        if(agent.getRadius() == agent.getMinRadius() && agent.distance(otherAgent) < 0)
+            return true;
+        double beta = getBeta(agent,otherAgent);
+        return beta >= 0 && beta <= Math.PI / 2 && parallelLinesIntersectAgentRadiusAndDistanceIsValid(agent,otherAgent);
+    }
+
+    public static boolean parallelLinesIntersectAgentRadiusAndDistanceIsValid(Agent agent, Agent otherAgent) {
+//        https://math.stackexchange.com/questions/422602/convert-two-points-to-line-eq-ax-by-c-0
+//        https://math.stackexchange.com/questions/1481904/distance-between-line-and-circle
+//        Aca, en vez de calcular para las dos lineas paralelas me fijo si la distancia
+//        Entre el circulo y la linea menos el radio del agente es menor que 0
+//        Es distinto a lo que dice el paper asi que checkear con Rafa
+//        Revisar esto y ver como hago para sacar el punto P que es el mas cercano, asi lo comparo con
+//        la distancia al centro del agente y me fijo que sea menor a 2*ri que es la segunda condicion del Ademas
+        Vector point1 = agent.getPosition();
+        Vector point2 = agent.getPosition().add(agent.getDirection());
+        Vector circleCenter = otherAgent.getPosition();
+        double circleRadius = otherAgent.getRadius();
+        return distanceLineFromCircle(point1,point2,circleCenter) - agent.getRadius() - circleRadius< 0;
+    }
+
+    private static double distanceLineFromCircle(Vector point1, Vector point2, Vector circleCenter) {
+        double A = point1.getY() - point2.getY();
+        double B = point2.getX() - point1.getX();
+        double C = point1.getX()*point2.getY() - point2.getX()*point1.getY();
+        return Math.abs(A*circleCenter.getX() + B*circleCenter.getY() + C)/Math.sqrt(A*A + B*B);
     }
 }
